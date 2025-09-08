71be7d3adf970e8134519b51e35a6f2356d3888e
Omar-RL <bloodstone.omh@gmail.com>
2025-09-08 17:26:32 -0500

    mejorando rutas en web.php (importaciones de clases)

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aquí registramos todas las rutas web de la aplicación.
+| Las rutas que requieren autenticación se agrupan con middleware 'auth'.
 |
 */
 
+// Ruta pública - página de inicio
 Route::get('/', function () {
     return view('estudiantes.index');
 });
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home')->middleware('auth');
-
+// Rutas de autenticación (login, register, logout, etc.)
 Auth::routes();
 
-Route::resource('estudiantes','App\Http\Controllers\EstudianteController');
+// Rutas protegidas por autenticación
+Route::middleware(['auth'])->group(function () {
+    
+    // Ruta al dashboard/home
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+
+    // Rutas para el CRUD de estudiantes
+    Route::resource('estudiantes', EstudianteController::class);
+
+});
