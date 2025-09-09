3520ef899bc9fe2d52c49b139f1482e6dd792f53
Omar-RL <bloodstone.omh@gmail.com>
2025-09-09 04:33:06 -0500

    Mejorando Show.blade.php

--- resources/views/estudiantes/show.blade.php
+++ resources/views/estudiantes/show.blade.php
@@ -3,45 +3,58 @@
 @section('title', 'Detalle del Estudiante')
 
 @section('content_header')
-    <h1>Detalle del Estudiante: {{ $estudiante->nombre }} {{ $estudiante->apellido }}</h1>
+    <h1>Detalle del Estudiante</h1>
 @stop
 
 @section('content')
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            <h3 class="box-title">Información del Estudiante</h3>
+    <div class="card card-primary card-outline">
+        <div class="card-header">
+            <h3 class="card-title">
+                <i class="fa fa-user-graduate"></i> {{ $estudiante->nombre }} {{ $estudiante->apellido }}
+            </h3>
         </div>
-        <div class="box-body">
-            <div class="form-group">
-                <label>ID Estudiante:</label>
-                <p class="form-control-static">{{ $estudiante->id }}</p>
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <strong><i class="fa fa-id-badge"></i> ID Estudiante:</strong>
+                    <p class="text-muted">{{ $estudiante->id }}</p>
+                </div>
+                <div class="col-md-6">
+                    <strong><i class="fa fa-envelope"></i> Email:</strong>
+                    <p class="text-muted">{{ $estudiante->email }}</p>
+                </div>
             </div>
-            <div class="form-group">
-                <label>Nombre:</label>
-                <p class="form-control-static">{{ $estudiante->nombre }}</p>
+
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <strong><i class="fa fa-user"></i> Nombre:</strong>
+                    <p class="text-muted">{{ $estudiante->nombre }}</p>
+                </div>
+                <div class="col-md-6">
+                    <strong><i class="fa fa-user"></i> Apellido:</strong>
+                    <p class="text-muted">{{ $estudiante->apellido }}</p>
+                </div>
             </div>
-            <div class="form-group">
-                <label>Apellido:</label>
-                <p class="form-control-static">{{ $estudiante->apellido }}</p>
-            </div>
-            <div class="form-group">
-                <label>Email:</label>
-                <p class="form-control-static">{{ $estudiante->email }}</p>
-            </div>
-            <div class="form-group">
-                <label>Cursos:</label>
-                @if($estudiante->cursos->isNotEmpty())
-                    <ul>
-                        @foreach($estudiante->cursos as $curso)
-                            <li>{{ $curso->nombre }}</li>
-                        @endforeach
-                    </ul>
-                @else
-                    <span class="badge badge-secondary">Sin cursos</span>
-                @endif
+
+            <div class="row">
+                <div class="col-12">
+                    <strong><i class="fa fa-book"></i> Cursos:</strong>
+                    @if($estudiante->cursos->isNotEmpty())
+                        <ul class="list-group list-group-flush mt-2">
+                            @foreach($estudiante->cursos as $curso)
+                                <li class="list-group-item">
+                                    <i class="fa fa-check-circle text-success"></i> {{ $curso->nombre }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    @else
+                        <span class="badge badge-secondary mt-2">Sin cursos asignados</span>
+                    @endif
+                </div>
             </div>
         </div>
-        <div class="box-footer">
+
+        <div class="card-footer">
             <a href="{{ route('estudiantes.index') }}" class="btn btn-default">
                 <i class="fa fa-arrow-left"></i> Volver
             </a>
@@ -57,5 +70,7 @@
 @stop
 
 @section('js')
-    <script> console.log("Show Estudiante page loaded."); </script>
+    <script>
+        console.log("Show Estudiante page loaded (versión profesional).");
+    </script>
 @stop
